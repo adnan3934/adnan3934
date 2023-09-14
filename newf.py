94397ddd1f489734dba1352d22b2c0ae66ddbd90
adnan3934 <144907246+adnan3934@users.noreply.github.com>
2023-09-14 20:41:40 +0530

    dictionary1.py

--- newf.py
+++ newf.py
@@ -1,3 +1,4 @@
+#create a dictionary and check username nad password found or not
 users = {'user1': 'password1', 'user2': 'password2', 'user3': 'password3', 'user4': 'password4', 'user5': 'password5'}
 
 username = input("Enter username: ")
